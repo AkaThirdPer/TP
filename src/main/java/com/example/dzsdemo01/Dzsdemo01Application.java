80d041d2f8fdd4b63a5420f4718a92edb2ccc90d
AkaThirdPer <42661823+AkaThirdPer@users.noreply.github.com>
2019-07-17 20:06:36 +0800

    Dzs第一次Commit

---
+++ src/main/java/com/example/dzsdemo01/Dzsdemo01Application.java
@@ -0,0 +1,13 @@
+package com.example.dzsdemo01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dzsdemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dzsdemo01Application.class, args);
+	}
+
+}
